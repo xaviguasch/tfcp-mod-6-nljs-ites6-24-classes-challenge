e0635d282717886f1b84eb9ebcf2cf38814740ef
xaviguasch <xaviguasch@gmail.com>
2020-08-06 12:47:04 +0200

    added solution

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log('testing')
-
 /* 
 **Classes Challenge**:
 
@@ -13,3 +11,31 @@ Create a Subclass called TennisPlayer that extends from the class Player
 - Add a function that when it runs should print into the console something similar ("Rafael Nadal is 34 years old and knows how to play Tennis");
 - Make sure the Name and Age are dynamic.
  */
+
+class Player {
+  constructor(name, country) {
+    this.name = name
+    this.country = country
+  }
+
+  gimmeMe() {
+    return `${this.name} was born in ${this.country}`
+  }
+}
+
+class TennisPlayer extends Player {
+  constructor(name, country, age) {
+    super(name, country)
+    this.age = age
+  }
+
+  informMe() {
+    return `${this.name} is ${this.age} old and knows how to play tennis.`
+  }
+}
+
+const barPlayer = new Player('Messi', 'Argentina')
+const tenPlayer = new TennisPlayer('Rafa Nadal', 'Spain', '35')
+
+console.log(barPlayer.gimmeMe())
+console.log(tenPlayer.informMe())
